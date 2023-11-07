7371739b4afd4a8cd7a06a8656dfdf0cee10cdd4
Рахат Базарбай <bazarbay.rahat@outlook.com>
2023-11-08 02:27:35 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Skills from "@/components/main/Skills"
+import Hero from "../components/main/Hero"
+import SkillDateProvider from "@/components/main/sub/SkillDateProvider"
+import Image from "next/image"
+import Encryption from "@/components/main/Encryption"
+import Projects from "@/components/main/Projects"
+
+export default function Home() {
+  return (
+    <main className='h-full w-full'>
+      <div className='flex flex-col gap-20'>
+        <Hero />
+        <Skills />
+        <Encryption />
+        <Projects />
+      </div>
+    </main>
+  )
+}
